8592bbed733398c9fbad5899f99a81e157e24eb9
Jonochi <tana.jayne@gmail.com>
2023-04-03 17:16:47 -0600

    some styling, search history funtionality

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,40 @@ let searchInput = document.getElementById("searchInput");
 let searchHistory = document.getElementById("searchHistory");
 let current = document.getElementById("current");
 let forecast = document.getElementById("forecast");
+let storedSearches;
+
+function renderLocalStorage() {
+    searchHistory.innerHTML = "";
+
+    for (let i = 0; i < storedSearches.length; i++) {
+
+        let searchHistoryDiv = document.createElement("div");
+        searchHistoryDiv.className = "searchHistoryDiv";
+        let searchHistoryBtn = document.createElement("button");
+        searchHistoryBtn.textContent = storedSearches[i];
+        searchHistoryDiv.append(searchHistoryBtn);
+        searchHistory.append(searchHistoryDiv);
+
+    }
+}
+
+function getLocalStorage() {
+    storedSearches = [];
+    storedSearches = JSON.parse(localStorage.getItem("searches")) || [];
+    renderLocalStorage();
+}
+
+getLocalStorage();
 
 //Current weather conditions
 function renderCurrentWeather(data) {
+    current.innerHTML = "";
     let name = document.createElement("div");
     name.textContent = data.name;
     current.append(name);
-    
+
     let date = document.createElement("div");
-    date.textContent = new Date(data.dt * 1000).toLocaleTimeString("en-US", {weekday: "long", year: "numeric", month: "short", day: "numeric"});
+    date.textContent = new Date(data.dt * 1000).toLocaleTimeString("en-US", { weekday: "long", year: "numeric", month: "short", day: "numeric" });
     current.append(date);
 
     let temp = document.createElement("div");
@@ -43,11 +68,10 @@ function displayCurrent(name) {
             return res.json()
         })
         .then(function (data) {
-            console.log(data);
             renderCurrentWeather(data);
         })
         .catch(function (err) {
-            console.log(error)
+            console.log(err)
         });
 }
 
@@ -56,21 +80,56 @@ let forecastHigh = -1000;
 let forecastLow = 1000;
 let avgWindspeed = 0;
 let avgHumidity = 0;
-let forecastIconSrc;
 
 function renderForecast(data) {
-    for (let i = 0; i < data.list; i++) {
+
+    forecast.innerHTML = "";
+    let infoIndex = [7, 15, 23, 31, 39];
+    for (let i = 0; i < data.list.length; i++) {
         let forecastCard = document.createElement("div");
         let forecastIcon = document.createElement("img");
 
-        if(data.list[i].main.temp_max > forecastHigh) {
-            forecastHigh = data.list[i].main.temp_max
+        if (data.list[i].main.temp_max > forecastHigh) {
+            forecastHigh = data.list[i].main.temp_max;
         }
-        if(data.list[i].main.temp_min < forecastLow) {
-            forecastLow = data.list[i].main.temp_min
+        if (data.list[i].main.temp_min < forecastLow) {
+            forecastLow = data.list[i].main.temp_min;
+        }
+
+        avgHumidity = avgHumidity + data.list[i].main.humidity;
+        avgWindspeed = avgWindspeed + data.list[i].wind.speed;
+
+        if (infoIndex.includes(i)) {
+            hiTemp = document.createElement("div")
+            hiTemp.textContent = "High: " + parseInt(forecastHigh)
+            forecastCard.append(hiTemp)
+            forecastHigh = -1000
+
+            lowTemp = document.createElement("div");
+            lowTemp.textContent = "Low: " + parseInt(forecastLow);
+            forecastCard.append(lowTemp);
+            forecastLow = 1000;
+
+            humidity = document.createElement("div");
+            humidity.textContent = "Humidity: " + parseInt(avgHumidity / 8);
+            forecastCard.append(humidity);
+            avgHumidity = 0;
+
+            wind = document.createElement("div");
+            wind.textContent = "Wind: " + parseInt(avgWindspeed / 8);
+            forecastCard.append(wind);
+            avgWindspeed = 0;
+
+            forecastIcon.src = "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
+            forecastCard.append(forecastIcon);
+
+            forecast.append(forecastCard);
+
         }
     }
 }
+
+
 
 function displayForecast(name) {
     let cityName = name;
@@ -82,18 +141,33 @@ function displayForecast(name) {
             return res.json()
         })
         .then(function (data) {
-            console.log(data);
             renderForecast(data);
         })
         .catch(function (err) {
-            console.log(error)
+            console.log(err)
         });
 }
 
+
+// Search city and city history
 function searchCity(e) {
     e.preventDefault();
     displayCurrent(searchInput.value);
     displayForecast(searchInput.value);
+    storedSearches.push(searchInput.value);
+    localStorage.setItem("searches", JSON.stringify(storedSearches));
+    getLocalStorage();
 }
+
+function searchCityFromHistory(name) {
+    displayCurrent(name);
+    displayForecast(name);
+}
+
+searchHistory.addEventListener("click", function (e) {
+    console.log(e.target.textContent);
+    searchCityFromHistory(e.target.textContent);
+})
+
 
 searchForm.addEventListener("submit", searchCity)
